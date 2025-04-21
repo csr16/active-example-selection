48dd8bbdca41a025e68acbf5f41c071c7ee38ac7
csr16 <sirui6@illinois.edu>
2025-04-20 21:51:12 -0500

    update step 2

--- batch_run_active_experiment.sh
+++ batch_run_active_experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=2,3
 
-datasets=(agnews sst-2 trec amazon)
-seeds=(0 1 2 3 4)  
+datasets=(agnews sst-2 trec amazon winowhy epistemic_reasoning hyperbaton timedial aqua)
+seeds=(0 1 2)  
 
 mkdir -p logs
 #####################
@@ -23,6 +23,24 @@ for dataset in "${datasets[@]}"; do
     log_file="logs/log_${dataset}_${seed}.txt"
     python src/rl/main.py "${temp_yaml}" >> "${log_file}" 2>&1
     
+    rm "${temp_yaml}"
+  done
+done
+
+for dataset in "${datasets[@]}"; do
+  for seed in "${seeds[@]}"; do
+    temp_yaml="rl_configs/temp_agnews_same_${dataset}_${seed}.yaml"
+    cp rl_configs/agnews-same-task.yaml "${temp_yaml}"
+
+    sed -i "s/^seed: .*/seed: ${seed}/" "${temp_yaml}"
+    sed -i "s|^\(\s*load_transitions:\).*|\1 ./outputs/active/${dataset}_${seed}/ckpts/transitions_2000.ckpt|" "${temp_yaml}"
+    sed -i "s/^\(\s*task:\).*/\1 ${dataset}/" "${temp_yaml}"
+
+    sed -i "s|^output_dir: .*|output_dir: ./outputs/active/${dataset}_${seed}|" "${temp_yaml}"
+
+    log_file="logs/log_same_${dataset}_${seed}.txt"
+
+    python src/rl/main.py "${temp_yaml}" >> "${log_file}" 2>&1
     rm "${temp_yaml}"
   done
 done
